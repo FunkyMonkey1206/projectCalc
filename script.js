ed147d33e789ae6d77220e1c43292ee746d905b4
FunkyMonkey1206 <FunkyMonkey1206@users.noreply.github.com>
2023-10-18 09:32:42 +0900

    Add operatior functions

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function add(firstVal, SecondVal) {
-    return firstVal + SecondVal;
+//Add functions for each operation
+function add(firstVal, secondVal) {
+    return firstVal + secondVal;
 }
+
+function subtract(firstVal, secondVal) {
+    return firstVal - secondVal;
+}
+
+function multiply(firstVal, secondVal) {
+    return firstVal * secondVal;
+}
+
+function divide(firstVal, secondVal) {
+    return firstVal / secondVal;
+}
+//Calculator operation
+//flow 
+//get first value 
+//select operator
+//input second value
+
+//Create Operate function that takes 
