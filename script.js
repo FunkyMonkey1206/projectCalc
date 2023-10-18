0b8c07d798fe9475f9f65f10f423ef11c0a674e7
FunkyMonkey1206 <FunkyMonkey1206@users.noreply.github.com>
2023-10-18 15:06:39 +0900

    Added basic calc functions and event listeners

--- script.js
+++ script.js
@@ -1,27 +1,138 @@
 //Add functions for each operation
-function add(firstVal, secondVal) {
-    alert(firstVal + secondVal);
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
+let currentVal = 0;
+let nextVal = 0;
+let operator = "";
+let valueHolder = "0";
+let result = 0;
+let dPointCount = 0;
+
+buttons.forEach((button) =>{
+    button.addEventListener('click', () => {
+        if(button.id == 'AC') {
+            allClear();
+        } else if(button.id == '+') {
+            if(operator != ""){
+                nextVal = +valueHolder
+                operate(currentVal, nextVal, operator)
+                outputDisplay(result);
+                currentVal = result;
+                valueHolder = "";
+                operator = button.id;
+            } else {
+                currentVal = +valueHolder;
+                operator = button.id;
+                valueHolder = "";
+            }
+        } else if(button.id == '-') {
+            if(operator != ""){
+                nextVal = +valueHolder
+                operate(currentVal, nextVal, operator)
+                outputDisplay(result);
+                currentVal = result;
+                valueHolder = "";
+                operator = button.id;
+            } else {
+                currentVal = +valueHolder;
+                operator = button.id;
+                valueHolder = "";
+            }
+        } else if(button.id == '×') {
+            if(operator != ""){
+                nextVal = +valueHolder
+                operate(currentVal, nextVal, operator)
+                outputDisplay(result);
+                currentVal = result;
+                valueHolder = "";
+                operator = button.id;
+            } else {
+                currentVal = +valueHolder;
+                operator = button.id;
+                valueHolder = "";
+            }
+        } else if(button.id == '÷') {
+            if(operator != ""){
+                nextVal = +valueHolder
+                operate(currentVal, nextVal, operator)
+                outputDisplay(result);
+                currentVal = result;
+                valueHolder = "";
+                operator = button.id;
+            } else {
+                currentVal = +valueHolder;
+                operator = button.id;
+                valueHolder = "";
+            }
+        } else if(button.id == "=") {
+            if(operator != ""){
+                nextVal = +valueHolder
+                operate(currentVal, nextVal, operator)
+                outputDisplay(result);
+                currentVal = result;
+                valueHolder = "";
+            } else {
+                outputDisplay(valueHolder);
+                valueHolder = "";
+            }
+        } else if(button.id == ".") {
+            if (dPointCount == 0) {
+                valueHolder += button.id;
+                outputDisplay(valueHolder);
+                dPointCount++;
+            } else {
+                outputDisplay(valueHolder);
+            }
+        } else {
+                valueHolder += button.id;
+                outputDisplay(valueHolder);
+        }
+        
+
+    })
+});
+
+function allClear() {
+    display.textContent = 0;
+    firstVal = 0;
+    secondVal = 0;
+    operator = "";
+    valueHolder = "";
+    dPointCount = 0;
+};
+
+function outputDisplay(input) {
+    display.textContent = input;
 }
 
-function subtract(firstVal, secondVal) {
-    alert(firstVal - secondVal);
+function clearDisplay() {
+    display.textContent = 0;
 }
 
-function multiply(firstVal, secondVal) {
-    alert(firstVal * secondVal);
-}
+// function add(firstVal, secondVal) {
+//     valueHolder = firstVal + secondVal;
+// }
 
-function divide(firstVal, secondVal) {
-    alert(firstVal / secondVal);
-}
+// function subtract(firstVal, secondVal) {
+//     valueHolder = firstVal - secondVal;
+// }
+
+// function multiply(firstVal, secondVal) {
+//     valueHolder = firstVal * secondVal;
+// }
+
+// function divide(firstVal, secondVal) {
+//     valueHolder = firstVal / secondVal;
+// }
 //Calculator operation
 //flow 
 //get first value 
-let firstVal = prompt("Input First Value");
-//select operator
-let operator = prompt("Input Operator");
-//input second value
-let secondVal = prompt("Input Second Value");
+// let firstVal = prompt("Input First Value");
+// //select operator
+// let operator = prompt("Input Operator");
+// //input second value
+// let secondVal = prompt("Input Second Value");
 
 //Create Operate function that takes value and perform desired operation
 function operate(firstVal, secondVal, operator) {
@@ -29,17 +140,23 @@ function operate(firstVal, secondVal, operator) {
     // operator = "-" ? subtract(firstVal, secondVal) : "Error";
     // operator = "*" ? multiply(firstVal, secondVal) : "Error";
     // operator = "/" ? divide(firstVal, secondVal) : "Error";
-
     
     if (operator == "+") {
-        add(firstVal, secondVal);
+        result = currentVal + nextVal;
     } else if (operator == "-") {
-        subtract(firstVal, secondVal);
-    } else if (operator == "*") {
-        multiply(firstVal, secondVal);
-    } else if (operator == "/") {
-        divide(firstVal, secondVal);
+        result = currentVal - nextVal;
+    } else if (operator == "×") {
+        result = currentVal * nextVal;
+    } else if (operator == "÷") {
+        if (nextVal == 0) {
+            alert("Snarky Error Message");
+            allClear();
+        } else {
+        result = currentVal / nextVal;
+        }
     }
+    dPointCount = 0;
 }
 
-operate(+firstVal, +secondVal, operator);
+clearDisplay();
+
